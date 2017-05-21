307ae2dbf77720bea0d4fb4986ab59995565a39e
jasonchiu0803 <jasonchiu0803@gmail.com>
2017-05-21 17:39:34 -0400

    new imputation

--- imputation.R
+++ imputation.R
@@ -1,6 +1,6 @@
 library(dplyr)
 library(data.table)
-setwd("/Users/jasonchiu0803/Desktop/data_bootcamp/Porject 3")
+setwd("/Users/jasonchiu0803/Desktop/data_bootcamp/sberbank_project")
 train <- fread("train.csv",stringsAsFactors = TRUE)
 
 # cleaning data
@@ -26,18 +26,18 @@ hist(train$year_cleaned)
 
 # replacing all missing values with the sub_are average
 # calculating average year that the apartments are built
-year_index_sub_area <- train %>% 
-  select(year_cleaned, sub_area) %>%
-  group_by(sub_area) %>%
-  summarise(avg_year = round(mean(year_cleaned, na.rm=TRUE)))
+#year_index_sub_area <- train %>% 
+#  select(year_cleaned, sub_area) %>%
+#  group_by(sub_area) %>%
+#  summarise(avg_year = round(mean(year_cleaned, na.rm=TRUE)))
 
 #replacing NAs with average from sub_area
-train1 <- left_join(train, year_index_sub_area,by = "sub_area")
-summary(train1$avg_year)
-na.index <- which(is.na(train$year_cleaned))
-train[na.index,"year_cleaned"] <- train1[na.index,"avg_year"]
-hist(train$year_cleaned)
-summary(train$year_cleaned)
+#train1 <- left_join(train, year_index_sub_area,by = "sub_area")
+#summary(train1$avg_year)
+#na.index <- which(is.na(train$year_cleaned))
+#train[na.index,"year_cleaned"] <- train1[na.index,"avg_year"]
+#hist(train$year_cleaned)
+#summary(train$year_cleaned)
 
 #State
 summary(train$state)
@@ -51,30 +51,31 @@ index_33
 train[index_33,"state_cleaned"] <- 3
 train[which(train$state_cleaned>4),"state_cleaned"] <- NA
 hist(train$state_cleaned)
-summary(train$state_cleaned)
 
 # kitch_sq
-train[which(train$kitch_sq==1970),]$build_year = 1970
+train[which(train$kitch_sq==1970),]$year_cleaned = 1970
 train[which(train$kitch_sq==1970),]$kitch_sq = NA
-train[which(train$kitch_sq==1974),]$build_year = 1974
+train[which(train$kitch_sq==1974),]$year_cleaned = 1974
 train[which(train$kitch_sq==1974),]$kitch_sq = NA
-train[which(train$kitch_sq==2013),]$build_year = 2013
+train[which(train$kitch_sq==2013),]$year_cleaned = 2013
 train[which(train$kitch_sq==2013),]$kitch_sq = NA
-train[which(train$kitch_sq==2014),]$build_year = 2014
+train[which(train$kitch_sq==2014),]$year_cleaned = 2014
 train[which(train$kitch_sq==2014),]$kitch_sq = NA
 train[which(train$kitch_sq==620),]$kitch_sq = NA
-train[which(train$kitch_sq > train$full_sq),]  <-  train[which(train$kitch_sq > train$full_sq),]$kitch_sq/10
+summary(train$year_cleaned)
+#train[which(train$kitch_sq > train$full_sq),]  <-  train[which(train$kitch_sq > train$full_sq),]$kitch_sq/10
 
 #full_sq cleaning
 train <- train %>% mutate(full_sq = replace(full_sq, full_sq == 0, NA))
 train <- train %>% mutate(full_sq = replace(full_sq, full_sq == 1, NA))
 train <- train %>% mutate(full_sq = replace(full_sq, full_sq == 5326, NA))
-train$full_sq[is.na(train$full_sq)] = mean(train$full_sq, na.rm=TRUE)
+#train$full_sq[is.na(train$full_sq)] = mean(train$full_sq, na.rm=TRUE)
+summary(train$year_cleaned)
 
 # life sq
 sum(train$life_sq > train$full_sq, na.rm=TRUE)
 train <- train %>% mutate(life_sq = replace(life_sq, life_sq == 7478, NA))
-train[which(train$life_sq > train$full_sq),]  <-  train[which(train$life_sq > train$full_sq),]$life_sq/10
+train[which(train$life_sq > train$full_sq),]$life_sq  <-  train[which(train$life_sq > train$full_sq),]$life_sq/10
 train[which(train$life_sq == 0),]$life_sq <- NA
 train[which(train$life_sq == 1),]$life_sq <- NA
 sum(is.na(train$life_sq))
@@ -120,9 +121,6 @@ first_set <- train %>% select(full_sq,
          material = factor(material,levels = c(1,2,3,4,5,6))) %>%
   select(-price_doc)
 
-summary(first_set$state_cleaned)
-summary(first_set$material)
-
 #linear regression
 #install.packages("VIF")
 #library(VIF)
@@ -139,34 +137,68 @@ summary(forwardAIC)
 backwardAIC = step(model.full, scope, direction = "backward", k = 2)
 summary(backwardAIC)
 bothAIC.empty = step(model.empty, scope, direction = "both", k = 2)
+summary(bothAIC.empty)
 bothAIC.full = step(model.full, scope, direction = "both", k = 2)
+summary(bothAIC.full)
+
+AIC(forwardAIC,
+    backwardAIC,
+    bothAIC.empty,
+    bothAIC.full)
+
+#Lasso
+#Need matrices for glmnet() function. Automatically conducts conversions as well
+#for factor variables into dummy variables.
+x = model.matrix(log_price ~ ., first_set_model)[, -1] #Dropping the intercept column.
+dim(x)
+y = first_set_model$log_price
+
+grid = 10^seq(3, -5, length = 100)
+
+#80 and 20 % train and test
+set.seed(0)
+train = sample(1:nrow(x), 8*nrow(x)/10)
+test = (-train)
+y.test = y[test]
+
+length(train)/nrow(x)
+length(y.test)/nrow(x)
+
+library(caret)
+library(plyr)
+dim(x)
+#dim(na.omit(x))
+#plyr::count(is.na(x))
+#sum(is.na(x))
+set.seed(0)
+train_control = trainControl(method = 'cv', number=10)
+#expand.grid
+tune.grid = expand.grid(lambda = grid, alpha=c(1))
+lasso.caret = train(x[train, ], y[train],
+                    method = 'glmnet',
+                    trControl = train_control, tuneGrid = tune.grid)
+plot(lasso.caret, xTrans=log)
+log(lasso.caret$bestTune)
+# -6.30
 
 
+set.seed(0)
+cv.lasso.out = cv.glmnet(x[train, ], y[train],
+                         lambda = grid, alpha = 1, nfolds = 10)
+plot(cv.lasso.out, main = "Lasso Regression\n")
+bestlambda.lasso = cv.lasso.out$lambda.min
+bestlambda.lasso
+log(bestlambda.lasso)
+# -6.12
+
+lasso.models.train = glmnet(x[train, ], y[train], alpha = 1, lambda = bestlambda.lasso)
+lasso.models.train$beta
+
+lasso.models.caret = glmnet(x[train, ], y[train], alpha = 1, lambda = lasso.caret$bestTune)
+lasso.models.caret$beta
 
 
-#Checking the model summary and assumptions of the reduced model.
-summary(forwardAIC)
-plot(forwardAIC)
-influencePlot(forwardAIC)
-vif(forwardAIC)
-avPlots(forwardAIC)
-confint(forwardAIC)
-
-#Predicting new observations.
-forwardAIC$fitted.values #Returns the fitted values.
-
-newdata = data.frame(Murder = c(1.5, 7.5, 12.5),
-                     HS.Grad = c(60, 50, 40),
-                     Frost = c(75, 55, 175),
-                     Population = c(7500, 554, 1212))
-
-predict(forwardAIC, newdata, interval = "confidence") #Construct confidence intervals
-#for the average value of an
-#outcome at a specific point.
-
-
-
-
+summary(lasso.caret)
 
 
 
